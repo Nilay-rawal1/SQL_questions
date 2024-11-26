2d5d98b6816a4c36c0886cd5c19266eda5618e7b
Nilay <rawalnilay1@gmail.com>
2024-11-26 10:35:29 +0530

    employee earning more than maneger

---
+++ 181_employee_earning.sql
@@ -0,0 +1,25 @@
+select e2.name  as Employee
+from employee e1
+INNER JOIN employee e2 on e1.id=e2.managerID
+where e1.salary<e2.salary
+
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The result format is in the following exampl
